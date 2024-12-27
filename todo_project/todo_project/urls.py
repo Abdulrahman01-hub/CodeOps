f0cbe0a5072914efafe42395fadda6292ddfbbd0
Abdulrahman01-hub <barbarossa.123456.1@gmail.com>
2024-12-27 12:50:28 +0300

    Update urls.py

--- todo_project/todo_project/urls.py
+++ todo_project/todo_project/urls.py
@@ -21,7 +21,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.task_list, name='task_list'),
-    path('add/', views.add_task, name='add_task'),
     path('update/<int:task_id>/', views.update_task, name='update_task'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
 ]
